4cedf5a62c8745342fb239248aac39aa2d956065
borisey <slamsnv@gmail.com>
2024-12-12 00:41:54 +0300

    client

--- main_client_service/src/main/java/com/example/client/controllers/DemoController.java
+++ main_client_service/src/main/java/com/example/client/controllers/DemoController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @GetMapping(value = "/hello")
-    public String getHello() {
+    @GetMapping(value = "/")
+    public String getMain() {
         return "Hello World, Docker!";
     }
 }
