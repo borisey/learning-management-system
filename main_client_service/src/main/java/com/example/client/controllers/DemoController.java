03daf07974889db980ed6fdbe84a64a2838099de
borisey <slamsnv@gmail.com>
2024-12-12 20:16:07 +0300

    fix

--- main_client_service/src/main/java/com/example/client/controllers/DemoController.java
+++ main_client_service/src/main/java/com/example/client/controllers/DemoController.java
@@ -2,17 +2,24 @@ package com.example.client.controllers;
 
 import com.example.client.service.HelloClientService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    private final HelloClientService grpcClient;
+
+    public DemoController(HelloClientService grpcClient) {
+        this.grpcClient = grpcClient;
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name) {
+        return grpcClient.sayHello(name);
+    }
 
     @GetMapping(value = "/")
     public String getMain() {
-
-//        HelloClientService helloClientService = new HelloClientService();
-//        String textMessage = helloClientService.sayHello("alex");
-
         return "textMessage";
     }
 }
