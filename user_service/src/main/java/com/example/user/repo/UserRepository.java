792f44122bb6bd2f96668f85a970a3014ad7f703
borisey <slamsnv@gmail.com>
2024-12-13 20:48:03 +0300

    fix

--- user_service/src/main/java/com/example/user/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.user.repo;
-
-import com.example.user.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
