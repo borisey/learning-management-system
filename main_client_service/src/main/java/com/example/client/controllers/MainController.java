c89d428077d0c24551922a3acfb76f96be09e0e3
borisey <slamsnv@gmail.com>
2024-12-14 18:04:08 +0300

    getUsers

--- main_client_service/src/main/java/com/example/client/controllers/MainController.java
+++ main_client_service/src/main/java/com/example/client/controllers/MainController.java
@@ -1,9 +1,12 @@
 package com.example.client.controllers;
 
 import com.example.client.service.HelloClientService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -20,5 +23,18 @@ public class MainController {
         model.addAttribute("users", users);
 
         return "main";
+    }
+
+    @PostMapping("/user/add")
+    public String userAdd(
+            HttpServletRequest request,
+            @RequestParam String name
+    ) {
+
+        grpcClient.sayHello(name);
+
+        String referrer = request.getHeader("Referer");
+
+        return "redirect:" + referrer;
     }
 }
