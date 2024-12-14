78ee0c1f86db7ac49e3d2e68bf96135d7aea1dc8
borisey <slamsnv@gmail.com>
2024-12-14 21:04:04 +0300

    fix

--- main_client_service/src/main/java/com/example/client/controllers/MainController.java
+++ main_client_service/src/main/java/com/example/client/controllers/MainController.java
@@ -1,9 +1,19 @@
 package com.example.client.controllers;
 
+import com.example.client.models.User;
 import com.example.client.service.HelloClientService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.catalina.connector.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +21,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController {
@@ -22,10 +35,12 @@ public class MainController {
     }
 
     @GetMapping(value = "/")
-    public String getMain(Model model) {
+    public String getMain(Model model) throws JsonProcessingException, JSONException {
         String users = grpcClient.getUsers();
 
-        model.addAttribute("users", users);
+        ArrayList<User> list = new Gson().fromJson(users, new TypeToken<ArrayList<User>>(){}.getType());
+
+        model.addAttribute("users", list);
 
         return "main";
     }
